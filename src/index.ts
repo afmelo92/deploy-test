f0464d4433f6abf80e6582382191ee0c0de57dc8
Andre <andre.fabian.melo@gmail.com>
2025-04-20 11:50:34 -0300

    Adding types to requests

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import express, { Express } from 'express';
+import express, { Express, Request, Response } from 'express';
 
 const app: Express = express();
 const port: number = Number(process.env.PORT) || 8080;
@@ -7,7 +7,14 @@ const port: number = Number(process.env.PORT) || 8080;
 app.use(express.json());
 
 // Rotas
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
+  res.json({
+    message: `Welcome to the new deploy in ${process.env.NODE_ENV}`,
+  });
+});
+
+// Rotas
+app.get('/', (req: Request, res: Response) => {
   res.json({
     message: `Welcome to the new deploy in ${process.env.NODE_ENV}`,
   });
