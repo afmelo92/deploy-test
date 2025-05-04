7aa4755eddebd794818d20d4d0e70da515001152
Andre <andre.fabian.melo@gmail.com>
2025-05-03 21:40:56 -0300

    Add husky

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,14 @@ import express, { Express, Request, Response } from 'express';
 
 const app: Express = express();
 
-const port: number = Number(process.env.PORT as string) || 8080;
+const port: number = Number(process.env.PORT) || 8080;
 
 app.use(express.json());
 
 // Rotas
 app.get('/', (req: Request, res: Response) => {
   res.json({
-    message: `Welcome to the new deploy in ${process.env.NODE_ENV as string}`,
-  });
-});
-
-// Rotas
-app.get('/', (req: Request, res: Response) => {
-  res.json({
-    message: `Welcome to the new deploy in ${process.env.NODE_ENV as string}`,
+    message: `Welcome to the new deploy in ${process.env.NODE_ENV}`,
   });
 });
 
